a64c51217fd0e378a3ce111763162eb6a0940406
seyha <yseyha@gmail.com>
2022-05-09 13:40:03 +0700

    add react js to prod

--- layouts/header.js
+++ layouts/header.js
@@ -3,7 +3,7 @@ function Header() {
     <>
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a
-          href="/"
+          href="/dev-toolkits"
           className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
         >
           <img
