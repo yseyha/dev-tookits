73c02b05fa8c16b22056e693162895b9d6d69a11
seyha <yseyha@gmail.com>
2022-05-09 22:11:02 +0700

    fixed path url

--- layouts/footer.js
+++ layouts/footer.js
@@ -1,41 +1,32 @@
 function Footer() {
   return (
     <>
-      <div className="text-center mt-5 mb-5">
+      {/* <div className="text-center mt-5 mb-5">
         <img
           src="https://images.placeholders.dev/?width=1080&height=150&text=Advertise%20here&bgColor=%23f7f6f6&textColor=%236d6e71"
           className="d-block mx-auto rounded text-center img-fluid"
           loading="lazy"
         />
-      </div>
+      </div> */}
       <footer className="py-3 my-4">
         <ul className="nav justify-content-center border-bottom pb-3 mb-3">
           <li className="nav-item">
-            <a href="/dev-toolkits" className="nav-link px-2 text-muted">
+            <a href="/" className="nav-link px-2 text-muted">
               Home
             </a>
           </li>
           <li className="nav-item">
-            <a
-              href="/dev-toolkits/privacy.html"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="/privacy" className="nav-link px-2 text-muted">
               Privacy & Policy
             </a>
           </li>
           <li className="nav-item">
-            <a
-              href="/dev-toolkits/terms.html"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="/terms" className="nav-link px-2 text-muted">
               Terms & Conditions
             </a>
           </li>
           <li className="nav-item">
-            <a
-              href="/dev-toolkits/contact.html"
-              className="nav-link px-2 text-muted"
-            >
+            <a href="/contact" className="nav-link px-2 text-muted">
               Contact
             </a>
           </li>
--- layouts/header.js
+++ layouts/header.js
@@ -3,7 +3,7 @@ function Header() {
     <>
       <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom">
         <a
-          href="/dev-toolkits"
+          href="/"
           className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
         >
           <img
@@ -14,31 +14,22 @@ function Header() {
 
         <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
           <li>
-            <a href="/dev-toolkits" className="nav-link px-2 link-secondary">
+            <a href="/" className="nav-link px-2 link-secondary">
               Home
             </a>
           </li>
           <li>
-            <a
-              href="/dev-toolkits/privacy"
-              className="nav-link px-2 link-secondary"
-            >
+            <a href="/privacy" className="nav-link px-2 link-secondary">
               Privacy & Policy
             </a>
           </li>
           <li>
-            <a
-              href="/dev-toolkits/terms"
-              className="nav-link px-2 link-secondary"
-            >
+            <a href="/terms" className="nav-link px-2 link-secondary">
               Terms & Conditions
             </a>
           </li>
           <li>
-            <a
-              href="/dev-toolkits/contact"
-              className="nav-link px-2 link-secondary"
-            >
+            <a href="/contact" className="nav-link px-2 link-secondary">
               Contact
             </a>
           </li>
@@ -56,13 +47,13 @@ function Header() {
           </a>
         </div>
       </header>
-      <div className="text-center mt-5 mb-5">
+      {/* <div className="text-center mt-5 mb-5">
         <img
           src="https://images.placeholders.dev/?width=1080&height=150&text=Advertise%20here&bgColor=%23f7f6f6&textColor=%236d6e71"
           className="d-block mx-auto rounded text-center img-fluid"
           loading="lazy"
         />
-      </div>
+      </div> */}
     </>
   );
 }
